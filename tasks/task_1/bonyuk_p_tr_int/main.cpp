c08facf18cdd54b320ce334c1bb4b6681239a344
PetrBonyuk <152027126+PetrBonyuk@users.noreply.github.com>
2023-12-23 18:56:09 +0300

    123542e

--- tasks/task_1/bonyuk_p_tr_int/main.cpp
+++ tasks/task_1/bonyuk_p_tr_int/main.cpp
@@ -19,62 +19,6 @@ TEST(TrapecIntegral, Fconst) {
 	}
 }
 
-TEST(TrapecIntegral, standardfun) {
-	boost::mpi::communicator world;
-	const int N = 1000;
-	const double a = -1, b = 1;
-	const double real_var = M_PI / 2;
-
-	double global_sum = TrapecIntegr(a, b, standard_function, N);
-
-	if (world.rank() == 0) {
-		ASSERT_NEAR(global_sum, real_var, ERRORRATE);
-	}
-}
-
-TEST(TrapecIntegral, complfun) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	const int N = 1000;
-	const double a = -1;
-	const double b = 1;
-	const double real_var = 0.230422;
-
-	double global_sum = TrapecIntegr(a, b, complex_function, N);
-
-	if (world.rank() == 0) {
-		ASSERT_NEAR(global_sum, real_var, ERRORRATE);
-	}
-}
-
-TEST(TrapecIntegral, sqrtfun) {
-	boost::mpi::communicator world;
-	const int N = 1000;
-	const double a = -1;
-	const double b = 1;
-	const double real_var = 329.363;
-
-	double global_sum = TrapecIntegr(a, b, complex_sqrt_function, N);
-
-	if (world.rank() == 0) {
-		ASSERT_NEAR(global_sum, real_var, ERRORRATE);
-	}
-}
-
-TEST(TrapecIntegral, sincosfun) {
-	boost::mpi::communicator world;
-	const int N = 1000;
-	const double a = -3.141592653589793;
-	const double b = 3.141592653589793;
-	const double real_var = 4.44288;
-
-	double global_sum = TrapecIntegr(a, b, complex_sin_cos_function, N);
-
-	if (rank == 0) {
-		ASSERT_LT(std::abs(real_var - global_sum), ERRORRATE);
-	}
-}
-
 int main(int argc, char** argv) {
 	boost::mpi::environment env(argc, argv);
 	boost::mpi::communicator world;
